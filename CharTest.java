19ae8bb2e4b40c7ef1bfd60317dcb7312512a042
shchfeng@163.com <shchfeng@163.com>
2017-12-07 21:29:57 +0800

    java codes for Crazy JAVA

---
+++ CharTest.java
@@ -0,0 +1,18 @@
+public class CharTest{
+    public static void main(String[] args){
+       char aChar = 'a';
+       char enterChar = '\r';
+       char ch = '\u9999';
+       System.out.println(aChar); 
+       System.out.println(enterChar);
+       System.out.println(ch);
+      
+       char zhong = 'ท่';
+       System.out.println(zhong);
+       int zhongValue = zhong;
+       System.out.println(zhongValue);
+
+       char c = 97;
+       System.out.println(c);
+}
+}
