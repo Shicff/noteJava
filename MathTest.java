19087916020991a6768c13d90f1da07334e42e2f
shchfeng@163.com <shchfeng@163.com>
2018-03-05 22:42:57 +0800

    添加文档介绍API

--- MathTest.java
+++ MathTest.java
@@ -1,13 +1,26 @@
+
+/**
+ *Description:
+ *<br>test
+ *<br>teeeeeeeeeeeeest
+ *<br>tttttttttttttttest
+ *<br>@version 1.0
+ */
 public class MathTest{
   public static void main(String[] args)
   {
+     /**
+      *简单做数学测试
+      */
      double a = 3.2;
      double b = Math.pow(a,5);
      System.out.println(b);
 
      double c = Math.sqrt(a);
      System.out.println(c);
-
+     /**
+      *简单再做数学测试
+      */
      double d = Math.random();
      System.out.println(d);
 
